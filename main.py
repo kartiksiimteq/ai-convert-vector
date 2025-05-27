ffdf217ef45a506a2e8d3991c15e08210d9c9bf3
Kartik Patel <kartik@siimteq.com>
2025-05-27 13:15:10 +0530

    wip

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ async def vector_from_image_url(payload: ImageURLRequest):
         })
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=5505, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5505, reload=True)
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5505)
+# if __name__ == '__main__':
+#     app.run(debug=False, host='0.0.0.0', port=5505)
